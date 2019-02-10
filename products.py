8a2f2c8d16b1d8a2d81f2561c7d87eff697e0793
Kenji Liu <kenjihell9563723@gmail.com>
2019-02-10 23:23:32 +0800

    add for loop for printing product

--- products.py
+++ products.py
@@ -5,7 +5,10 @@ while True:
 		break
 	price = input('請輸入商品價格: ')
 	p = [name, price]
-    products.append(p)
+	products.append(p)
 print(products)
+
+for p in products:
+	print(p[0], '的價格是', p[1])
 
 
